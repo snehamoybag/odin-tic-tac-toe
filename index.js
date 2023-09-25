e282d899eeacde2db4c901b4360eef4210d218ba
snehamoybag <snehamoybag2@gmail.com>
2023-09-25 23:54:06 +0530

    add game logic comments

---
+++ index.js
@@ -0,0 +1,7 @@
+// get players
+// show board on start
+// player can select certain cells to place their marker
+// check for  win or draw after player plays their turn
+// show win/draw message if  win/draw found
+// end game after win/draw
+// if no win/draw swith turn to other player
