2d5ed970855a47524cc0eb84f3eb5483a61c45a0
snehamoybag <snehamoybag2@gmail.com>
2023-09-26 20:27:42 +0530

    add screenController module functions

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// get players
 // show board on start
 // get players
 // player can select certain cells to place their marker
@@ -7,32 +6,44 @@
 // end game after win/draw
 // if no win/draw swith turn to other player
 const Player = (name, marker) => {
+  let _currentCombo = "";
+
+  const setCurrentCombo = (val) => {
+    if (_currentCombo.length === 3) {
+      _currentCombo = "" + val;
+    } else {
+      _currentCombo += val;
+    }
+  };
+  const getCurrentCombo = () => _currentCombo;
+
   return {
     name,
     marker,
+    setCurrentCombo,
+    getCurrentCombo,
   };
 };
 
 const Cell = (row, column) => {
-  let _value = column;
+  const _value = column;
   let _isTaken = false;
+  let _marker;
 
-  const setValue = (val) => {
-    _value = val;
+  const take = () => {
+    _isTaken = take;
   };
   const getValue = () => _value;
-  const setTakenStatus = (bool) => {
-    _isTaken = bool;
-  };
   const getTakenStatus = () => _isTaken;
-  const getPosition = () => ({ row, column });
+  const setMarker = (marker) => {
+    _marker = marker;
+  };
 
   return {
-    setValue,
     getValue,
-    setTakenStatus,
+    take,
     getTakenStatus,
-    getPosition,
+    setMarker,
   };
 };
 
@@ -54,35 +65,94 @@ const GameBoard = () => {
 
     _board = freshBoard;
   };
+  const get = () => _board;
+  const getCellValue = (row, column) => _board[row][column].getValue();
+  const placeMarker = (row, column, marker) => {
+    _board[row][column].take();
+    _board[row][column].setMarker(marker);
+  };
+  const getFilledStatus = () => {
+    let isBoardFilled = true;
+
+    for (let i = 0; i < _board.length; i++) {
+      for (let j = 0; j < _board[i].length; j++) {
+        const currentCell = _board[i][j];
+        if (!currentCell.getTakenStatus()) {
+          isBoardFilled = false;
+          return isBoardFilled; // return immediately if empty cell is found
+        }
+      }
+    }
+
+    return isBoardFilled;
+  };
 
   // initialize default board state
   setDefaultState();
 
-  const get = () => _board;
-  const placeMarker = (player, row, column) => {
-    _board[row];
+  return {
+    setDefaultState,
+    get,
+    getCellValue,
+    placeMarker,
+    getFilledStatus,
+  };
+};
+
+const GameController = (player1, player2) => {
+  const _gameBoard = GameBoard();
+  let _currentPlayer = player1;
+
+  const getCurrentPlayer = () => _currentPlayer;
+  const switchPlayer = () => {
+    _currentPlayer = _currentPlayer === player1 ? player2 : player1;
+  };
+  const _getWinner = () => {
+    const winCombos = ["012", "000", "111", "222"];
+    if (winCombos.includes(_currentPlayer.getCurrentCombo()))
+      return _currentPlayer;
+  };
+  const checkWin = () => {
+    const isBoardFilled = _gameBoard.getFilledStatus();
+    const winner = _getWinner();
+
+    if (winner) return true;
+    if (isBoardFilled && !winner) return false;
+  };
+  const playRound = (row, column) => {
+    // before playing a round check if board is filled or not
+    // if filled return immediately
+    const isBoardFilled = _gameBoard.getFilledStatus();
+    const isCellAvailable = _gameBoard[row][column].getTakenStatus();
+
+    if (isBoardFilled || !isCellAvailable) return;
+
+    _gameBoard.placeMarker(row, column, _currentPlayer.marker);
   };
 
   return {
-    get,
-    setDefaultState,
+    getCurrentPlayer,
+    switchPlayer,
+    checkWin,
+    getGameBoard: _gameBoard.get(),
   };
 };
 
-const GameControl = () => {
-  const gameBoard = GameBoard();
+const ScreenController = () => {
+  const _player1 = Player("Snehamoy", "X");
+  const _player2 = Player("Futuman", "O");
+  const _gameController = GameController(_player1, _player2);
 
-  const checkWinner = () => {
-    const winCombos = ["012", "000", "111", "222"];
+  const renderGameBoard = () => {
+    const gameBoard = _gameController.getGameBoard();
+    console.log(gameBoard);
+  };
+  const renderEndScreen = (containerEl, msgEl, restartBtnEl) => {
+    console.log("The winner is " + _gameController.getCurrentPlayer());
   };
 
-  const playerX = Player("Player X", "X");
-  const playerO = Player("Player O", "O");
-  let currentPlayer = playerX;
-
-  const switchPlayer = () => {
-    currentPlayer = currentPlayer === playerX ? playerO : playerX;
+  return {
+    renderGameBoard,
+    renderEndScreen,
   };
 };
-
-gameBoard.render();
